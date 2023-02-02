d0c7fc0711d4aafa14c670d04aaad39f843a0ff9
FFilins <felipeclayton007@gmail.com>
2023-02-02 14:22:42 -0300

    Confirmando aprendizado no Github.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agora vai!</title>
+</head>
+<body>
+    <?php 
+    echo 'Nem tudo na vida são flores.';
+
+    echo 'Temos que ser fortes';
+
+    
+    ?>
+</body>
+</html>
